8190ee36c6a5ee032ede66700cf1eb05499808ff
Pascal Thomet <pthomet@gmail.com>
2022-11-27 12:17:57 +0100

    organize folders

--- README.md
+++ README.md
@@ -5,20 +5,33 @@ simple bindings for srcML, when used with C++ code.
 Provides:
 
 ````python
-def to_cpp(
-    xml_str: str,
-    encoding_src: str = "utf-8",
-    encoding_xml: str = "utf-8"
-    ) -> Optional[str]:
+from typing import Optional
+import enum
+
+
+class CodeLanguage(enum.Enum):
+    c = enum.auto()            # (= 0)
+    c_sharp = enum.auto()      # (= 1)
+    c_plus_cplus = enum.auto() # (= 2)
+    java = enum.auto()         # (= 3)
+    objective_c = enum.auto()  # (= 4)
+
+
+def to_code(
+        xml_str: str,
+        encoding_src: str = "utf-8",
+        encoding_xml: str = "utf-8"
+) -> Optional[str]:
     pass
 
 
 def to_srcml(
-    cpp_code: str,
-    include_positions: bool = True,
-    encoding_src: str = "utf-8",
-    encoding_xml: str = "utf-8"
-    ) -> Optional[str]:
+        code: str,
+        language: CodeLanguage,
+        include_positions: bool = True,
+        encoding_src: str = "utf-8",
+        encoding_xml: str = "utf-8"
+) -> Optional[str]:
     pass
 ````
 
--- autogenerate_srcmlcpp_caller.py
+++ autogenerate_srcmlcpp_caller.py
@@ -2,23 +2,17 @@ import os
 import litgen
 
 
-THIS_DIR = os.path.dirname(__file__)
-EXTERNAL_DIR = THIS_DIR + "/external"
-CPP_LIB_DIR = EXTERNAL_DIR + "/libsrcml_caller"
-CPP_GENERATED_PYBIND_DIR = THIS_DIR + "/bindings"
-assert os.path.isdir(CPP_LIB_DIR)
-assert os.path.isdir(CPP_GENERATED_PYBIND_DIR)
 
 
 def autogenerate():
-    output_cpp_pydef_file = CPP_GENERATED_PYBIND_DIR + "/pybind_srcml_caller.cpp"
-    output_stub_pyi_file = CPP_GENERATED_PYBIND_DIR + "/srcml_caller/__init__.pyi"
+    this_dir = os.path.dirname(__file__)
+    output_cpp_pydef_file = this_dir + "/bindings/pybind_srcml_caller.cpp"
+    output_stub_pyi_file = this_dir + "/bindings/srcml_caller/__init__.pyi"
 
     # Configure options
     options = litgen.LitgenOptions()
 
-    include_dir = THIS_DIR + "/external/libsrcml_caller"
-    header_file = include_dir + "/libsrcml_caller.h"
+    header_file = this_dir + "/src/libsrcml_caller/libsrcml_caller.h"
     litgen.write_generated_code_for_file(options, header_file, output_cpp_pydef_file, output_stub_pyi_file)
 
 
