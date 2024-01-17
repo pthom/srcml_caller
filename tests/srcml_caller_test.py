29852afbb844f96bd6fafd1e770a2640b41f8d34
Pascal Thomet <pthomet@gmail.com>
2024-01-17 17:51:55 +0100

    fix test

--- tests/srcml_caller_test.py
+++ tests/srcml_caller_test.py
@@ -2,7 +2,7 @@ import srcml_caller
 
 
 def test_version():
-    assert srcml_caller.__version__ == "0.1.2"
+    assert srcml_caller.__version__ == "0.1.3"
 
 def test_code():
     import srcml_caller
